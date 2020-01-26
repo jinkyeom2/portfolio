107d1e7eacb97b8733ec9a3eafa033b24634a21e
jklee <jklee@jkleeui-MacBook-Pro.local>
2020-01-26 21:18:35 +0900

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+const gulp         = require('gulp');
+const browserSync  = require('browser-sync').create();
+const sass         = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const root_path = "/Users/jklee/Desktop/jklee/"
+// Compile Sass & Inject Into Browser
+gulp.task('sass', function() {
+    return gulp.src([root_path + 'src/scss/*.scss'])
+        .pipe(sass())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest(root_path + "src/css"))
+        .pipe(browserSync.stream());
+});
+
+
+// Watch Sass & Serve
+gulp.task('serve', ['sass'], function() {
+    browserSync.init({
+        server: "/Users/jklee/Desktop/jklee/src"
+    });
+
+    gulp.watch([root_path + 'src/scss/*.scss'], ['sass']);
+    gulp.watch(root_path + "src/*.html").on('change', browserSync.reload);
+});
+
+// Default Task
+gulp.task('default', ['serve']);
