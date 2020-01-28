c0611de379b1caa0b75527d3eb7452326af32e33
jklee <jklee@jkleeui-MacBook-Pro.local>
2020-01-28 20:23:04 +0900

    modify

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,17 @@ const gulp         = require('gulp');
 const browserSync  = require('browser-sync').create();
 const sass         = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
-const root_path = "/Users/jklee/Desktop/jklee/"
+//const root_path = "/Users/jklee/Desktop/jklee/"
+const root_path = "./";
 const deploy = require('gulp-gh-pages');
 // Compile Sass & Inject Into Browser
 gulp.task('sass', function() {
     return gulp.src([root_path + 'src/scss/*.scss'])
         .pipe(sass())
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
+        // .pipe(autoprefixer({
+        //     browsers: ['last 2 versions'],
+        //     cascade: false
+        // }))
         .pipe(gulp.dest(root_path + "src/css"))
         .pipe(browserSync.stream());
 });
@@ -20,11 +21,15 @@ gulp.task('sass', function() {
 // Watch Sass & Serve
 gulp.task('serve', ['sass'], function() {
     browserSync.init({
-        server: "/Users/jklee/Desktop/jklee/src"
+        //server: "/Users/jklee/Desktop/jklee/src"
+        server : {
+          baseDir: './src'
+        }
     });
 
-    gulp.watch([root_path + 'src/scss/*.scss'], ['sass']);
+    gulp.watch(root_path + 'src/scss/*.scss', ['sass']);
     gulp.watch(root_path + "src/*.html").on('change', browserSync.reload);
+    gulp.watch("./src/*/*.html").on('change', browserSync.reload);
 });
 
 // Default Task
